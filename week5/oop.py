ff131b02cab8a1bdc126bae24bcf5c7323eb2622
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-16 16:10:08 +0300

    k

--- week5/oop.py
+++ week5/oop.py
@@ -1,8 +1,16 @@
 class Polygon:
     # magic method
-    def __init(self,name,size):
+    def __init(self,name,length,width,size):
         name = self.name
+        length = self.length
+        width = self.width
         size = self.size
 
+    #creating a mehod
+    def discribe(self):
+        return f'name- {self.name}, length- {self.length}, width- {self.width}, size- {self.size}'
+    
+    def area(self):
+        pass
 
-polygon = Polygon()
+
