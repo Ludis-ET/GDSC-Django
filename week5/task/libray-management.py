0eff316eca79a8aec1d5a93451cd98a08aed7fd9
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-18 09:55:21 +0300

    last

--- week5/task/libray-management.py
+++ week5/task/libray-management.py
@@ -6,7 +6,7 @@ class Book:
         self.availability_status = True
 
     def display_details(self):
-        print(f"Title: {self.title}\nAuthor: {self.author}\nISBN: {self.isbn}\nAvailability: {'Available' if self.availability_status else 'Borrowed'}")
+        print(f"Title: {self.title}\nAuthor: {self.author}\nISBN: {self.isbn}\nAvailability: {'Available' if self.availability_status else 'Not Available'}\n")
 
 
 class User:
@@ -16,8 +16,23 @@ class User:
         self.books_borrowed = []
 
     def display_details(self):
-        print(f"User ID: {self.user_id}\nName: {self.name}\nBooks Borrowed: {', '.join(self.books_borrowed)}")
+        print(f"User ID: {self.user_id}\nName: {self.name}\nBooks Borrowed: {', '.join(self.books_borrowed)}\n")
 
+    def borrow_book(self, book):
+        if book.availability_status:
+            self.books_borrowed.append(book.title)
+            book.availability_status = False
+            print(f"{self.name} has successfully borrowed '{book.title}'.")
+        else:
+            print(f"Sorry, '{book.title}' is not available for borrowing.")
+
+    def return_book(self, book):
+        if book.title in self.books_borrowed:
+            self.books_borrowed.remove(book.title)
+            book.availability_status = True
+            print(f"{self.name} has successfully returned '{book.title}'.")
+        else:
+            print(f"You haven't borrowed '{book.title}'. Cannot return.")
 
 class Library:
     def __init__(self):
@@ -31,114 +46,32 @@ class Library:
 
     def register_user(self, user):
         self.users.append(user)
-        print(f"User '{user.name}' registered.")
+        print(f"User '{user.name}' registered in the library.")
 
     def borrow_book(self, user, book):
-        if book.availability_status:
-            book.availability_status = False
-            user.books_borrowed.append(book.title)
-            self.transactions.append({'user': user.name, 'book': book.title, 'action': 'Borrowed'})
-            print(f"{user.name} borrowed '{book.title}'.")
-        else:
-            print(f"Sorry, '{book.title}' is not available for borrowing.")
+        transaction = Transaction(user, book, "Borrowed")
+        self.transactions.append(transaction)
+        user.borrow_book(book)
 
     def return_book(self, user, book):
-        if book.availability_status:
-            print(f"Error: '{book.title}' is already available in the library.")
-        elif book.title in user.books_borrowed:
-            book.availability_status = True
-            user.books_borrowed.remove(book.title)
-            self.transactions.append({'user': user.name, 'book': book.title, 'action': 'Returned'})
-            print(f"{user.name} returned '{book.title}'.")
-        else:
-            print(f"Error: '{user.name}' did not borrow '{book.title}'.")
-
-    def generate_transaction_report(self):
-        print("\nTransaction Report:")
-        for transaction in self.transactions:
-            print(f"{transaction['user']} {transaction['action']} '{transaction['book']}'")
+        transaction = Transaction(user, book, "Returned")
+        self.transactions.append(transaction)
+        user.return_book(book)
 
     def display_books(self):
-        print("\nBook Details:")
+        print("Library Books:")
         for book in self.books:
             book.display_details()
 
     def display_users(self):
-        print("\nUser Details:")
+        print("Library Users:")
         for user in self.users:
             user.display_details()
 
-    def display_menu(self):
-        print("\nMenu:")
-        print("1. Add Book")
-        print("2. Register User")
-        print("3. Borrow Book")
-        print("4. Return Book")
-        print("5. Display Books")
-        print("6. Display Users")
-        print("7. Generate Transaction Report")
-        print("8. Exit")
 
-    def run_menu(self):
-        while True:
-            self.display_menu()
-            choice = input("Enter your choice (1-8): ")
+class Transaction:
+    def __init__(self, user, book, transaction_type):
+        self.user = user
+        self.book = book
+        self.transaction_type = transaction_type
 
-            if choice == "1":
-                title = input("Enter book title: ")
-                author = input("Enter author name: ")
-                isbn = input("Enter ISBN: ")
-                new_book = Book(title, author, isbn)
-                self.add_book(new_book)
-
-            elif choice == "2":
-                user_id = input("Enter user ID: ")
-                name = input("Enter user name: ")
-                new_user = User(user_id, name)
-                self.register_user(new_user)
-
-            elif choice == "3":
-                user_id = input("Enter user ID: ")
-                book_title = input("Enter book title: ")
-                user = next((u for u in self.users if str(u.user_id) == user_id), None)
-                book = next((b for b in self.books if b.title == book_title), None)
-
-                if user and book:
-                    self.borrow_book(user, book)
-                else:
-                    print("User or book not found.")
-
-            elif choice == "4":
-                user_id = input("Enter user ID: ")
-                book_title = input("Enter book title: ")
-                user = next((u for u in self.users if str(u.user_id) == user_id), None)
-                book = next((b for b in self.books if b.title == book_title), None)
-
-                if user and book:
-                    self.return_book(user, book)
-                else:
-                    print("User or book not found.")
-
-            elif choice == "5":
-                self.display_books()
-
-            elif choice == "6":
-                self.display_users()
-
-            elif choice == "7":
-                self.generate_transaction_report()
-
-            elif choice == "8":
-                print("Exiting the Library Management System. Goodbye!")
-                break
-
-            else:
-                print("Invalid choice. Please enter a number between 1 and 8.")
-
-
-def main():
-    library = Library()
-    library.run_menu()
-
-
-main()
