fb12df4fc2f4c3c141df96e82dfa49a055af4ebf
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-27 15:38:29 +0300

    creting models

--- blogger/blog/models.py
+++ blogger/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    body = models.TextField()
+    def __str__(self):
+        return self.title
