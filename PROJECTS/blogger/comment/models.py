86a8353e91a2322926f3144dc7f0407b5af13b52
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-11 16:07:05 +0300

    creating comment model

--- PROJECTS/blogger/comment/models.py
+++ PROJECTS/blogger/comment/models.py
@@ -3,4 +3,9 @@ from django.db import models
 # Create your models here.
 class Comment(models.Model):
     #content, created time, and modified time.
-    name = models.CharField
+    name = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_add=True)
+    modified_time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
