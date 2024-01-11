07a819f04221fba6318038c841aac349d6f75666
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-11 16:13:01 +0300

    applying migrations

--- PROJECTS/blogger/comment/models.py
+++ PROJECTS/blogger/comment/models.py
@@ -5,7 +5,7 @@ class Comment(models.Model):
     #content, created time, and modified time.
     name = models.CharField(max_length=255)
     content = models.TextField()
-    created_time = models.DateTimeField(auto_add=True)
+    created_time = models.DateTimeField(auto_now=True)
     modified_time = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
